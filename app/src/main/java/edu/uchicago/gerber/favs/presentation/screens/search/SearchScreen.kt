8926bc52c8c0b7dceddb158352745262458fd0bc
Adam Gerber <adam.gerber@yahoo.com>
2023-06-27 15:00:56 -0500

    implement SearchScreen with Internet permissions

--- app/src/main/java/edu/uchicago/gerber/favs/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/edu/uchicago/gerber/favs/presentation/screens/search/SearchScreen.kt
@@ -1,9 +1,14 @@
 package edu.uchicago.gerber.favs.presentation.screens.search
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,9 +23,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import edu.uchicago.gerber.favs.common.Constants
+import edu.uchicago.gerber.favs.presentation.screens.search.paging.BookList
+import edu.uchicago.gerber.favs.presentation.screens.search.paging.SearchOperation
 import edu.uchicago.gerber.favs.presentation.viewmodels.BookViewModel
 import edu.uchicago.gerber.favs.presentation.widgets.BottomNavigationBar
 import edu.uchicago.gerber.favs.presentation.widgets.CustomOutlinedTextField
@@ -31,6 +39,7 @@ fun SearchScreen(
     bookViewModel: BookViewModel,
     navController: NavController,
 ) {
+    val state = bookViewModel.searchState.value
     val queryText = bookViewModel.queryText.value
 
     Scaffold(
@@ -72,6 +81,31 @@ fun SearchScreen(
                 ImeAction.Search,
                 bookViewModel::onSearch
             )
+
+            Spacer(modifier = Modifier.height(10.dp))
+            when (state.searchOperation) {
+                SearchOperation.LOADING -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .align(
+                                    Alignment.Center
+                                )
+                        )
+                    }
+                }
+                SearchOperation.DONE -> {
+                    BookList(bookViewModel, navController)
+                }
+                else -> {
+                    Box {}
+                }
+            }
         }
     }
 }
--- app/src/main/java/edu/uchicago/gerber/favs/ui/theme/Theme.kt
+++ app/src/main/java/edu/uchicago/gerber/favs/ui/theme/Theme.kt
@@ -39,7 +39,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun FavoriteBooksTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    //todo set back to isSystemInDarkTheme()
+   // darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
