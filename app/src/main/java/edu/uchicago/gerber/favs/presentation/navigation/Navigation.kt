2be7574e36b2951862bb80c4ff4d7505f68236bf
Adam Gerber <adam.gerber@yahoo.com>
2023-06-27 14:36:26 -0500

    implement navigation and update screens

--- app/src/main/java/edu/uchicago/gerber/favs/presentation/navigation/Navigation.kt
+++ app/src/main/java/edu/uchicago/gerber/favs/presentation/navigation/Navigation.kt
@@ -1,6 +1,7 @@
 package edu.uchicago.gerber.favs.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -8,23 +9,25 @@ import edu.uchicago.gerber.favs.presentation.screens.contact.ContactScreen
 import edu.uchicago.gerber.favs.presentation.screens.details.DetailsScreen
 import edu.uchicago.gerber.favs.presentation.screens.favorites.FavoritesScreen
 import edu.uchicago.gerber.favs.presentation.screens.search.SearchScreen
+import edu.uchicago.gerber.favs.presentation.viewmodels.BookViewModel
 
 @Composable
 fun Navigation(
-    navController: NavHostController
+    navController: NavHostController,
+    bookViewModel: BookViewModel = viewModel()
 ) {
 
     NavHost(navController, startDestination = Screen.Search.route) {
         composable(Screen.Search.route) {
-            SearchScreen()
+            SearchScreen(bookViewModel, navController)
 
         }
         composable(Screen.Favorites.route) {
-            FavoritesScreen()
+            FavoritesScreen(navController)
         }
 
         composable(Screen.Contact.route) {
-            ContactScreen()
+            ContactScreen(navController)
         }
 
         composable(Screen.Detail.route) {
--- app/src/main/java/edu/uchicago/gerber/favs/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/edu/uchicago/gerber/favs/presentation/screens/search/SearchScreen.kt
@@ -1,8 +1,77 @@
 package edu.uchicago.gerber.favs.presentation.screens.search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import edu.uchicago.gerber.favs.common.Constants
+import edu.uchicago.gerber.favs.presentation.viewmodels.BookViewModel
+import edu.uchicago.gerber.favs.presentation.widgets.BottomNavigationBar
+import edu.uchicago.gerber.favs.presentation.widgets.CustomOutlinedTextField
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchScreen() {
+fun SearchScreen(
+    bookViewModel: BookViewModel,
+    navController: NavController,
+) {
+    val queryText = bookViewModel.queryText.value
 
+    Scaffold(
+        modifier = Constants.modifier,
+        bottomBar = { BottomNavigationBar(navController) },
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Transparent
+                ),
+                title = {
+                    Text(
+                        text = "Search Books",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        style = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp
+                    )
+
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues = paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            CustomOutlinedTextField(
+                title = "Search term(s)",
+                placeHolder = "e.g. java",
+                textState = queryText,
+                onTextChange = bookViewModel::setQueryText,
+                keyboardType = KeyboardType.Text,
+                ImeAction.Search,
+                bookViewModel::onSearch
+            )
+        }
+    }
 }
